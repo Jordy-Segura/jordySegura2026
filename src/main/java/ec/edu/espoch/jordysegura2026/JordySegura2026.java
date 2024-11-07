5cb908b05708941aafece4f0023162eceee89a0e
jordy <marcelo2005jmsp@gmail.com>
2024-11-07 13:17:22 -0500

    Leccion de Jordy Segura

---
+++ src/main/java/ec/edu/espoch/jordysegura2026/JordySegura2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.jordysegura2026;
+
+/**
+ *
+ * @author jordy
+ */
+public class JordySegura2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
