1eafffd07744afa39dc8b4c1d23c125bac5e0244
jordy <marcelo2005jmsp@gmail.com>
2024-11-09 08:33:36 -0500

    2da actualización

--- src/main/java/ec/edu/espoch/jordysegura2026/JordySegura2026.java
+++ src/main/java/ec/edu/espoch/jordysegura2026/JordySegura2026.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ec.edu.espoch.jordysegura2026;
 
 import java.util.Scanner;
@@ -13,63 +12,78 @@ import java.util.Scanner;
 public class JordySegura2026 {
 
     public static void main(String[] args) {
-        
-        double [] precioProducto;
-        
-        precioProducto=new double[5];
-        
-        double promedioPrecios = 0;
-        double precioAlto = 0;
-        double precioBajo = 0;
-        double contadorProductos = 0;
-           
-        Scanner precios=new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {            
-            System.out.println("Ingrese el precio del producto "+(i+1)+": ");
-            double precio=precios.nextDouble();
-            precioProducto[i]=precio;        
+
+        float[] precioProducto;
+
+        precioProducto = new float[5];
+
+        float promedioPrecios = 0;
+        float precioAlto = 0;
+        float precioBajo = 0;
+        int contadorProductos = 0;
+
+        Scanner precios = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ingrese el precio del producto " + (i + 1) + ": ");
+            float precio = precios.nextFloat();
+            precioProducto[i] = precio;
         }
         //precios.nextLine();
-        
-        System.out.println("-----INGRESE UNA OPCION DEL 1-5.-----");
-        System.out.println("1.  CALCULAR EL PRECIO PROMEDIO DE LOS PRODUCTOS.");
-        System.out.println("2.  MOSTRAR EL PRECIO MÁS ALTO.");
-        System.out.println("3.  CONTAR PRODUCTOS POR ENCIMA DEL PROMEDIO.");
-        System.out.println("4.  Salir");
-        
-        Scanner ingresoMenu=new Scanner(System.in);
-        int menu=ingresoMenu.nextInt();
-        
-        ingresoMenu.nextLine();
-        switch(menu){
-            case 1:
-                System.out.println("----CALCULAR EL PRECIO PROMEDIO DE LOS PRODUCTOS----");
-                double suma =0;
-                
-                for (int i = 0; i < 5; i++) {
-                    suma=precioProducto[i]+suma;
-                }
-                promedioPrecios=suma/5;
-                
-                System.out.println("El promedio de los precios es: "+promedioPrecios);
-                break;
-                
-            case 2:
-                precioAlto = precioProducto[0];
-                precioBajo = precioProducto[0];
-                
-                
-                break;
-                
-            case 3:
 
-                break;
-            case 4:
+        Scanner ingresoMenu = new Scanner(System.in);
+        int menu = 0;
+        while (menu!=4) {
+            System.out.println("-----INGRESE UNA OPCION DEL 1-5.-----");
+            System.out.println("1.  CALCULAR EL PRECIO PROMEDIO DE LOS PRODUCTOS.");
+            System.out.println("2.  MOSTRAR EL PRECIO MÁS ALTO.");
+            System.out.println("3.  CONTAR PRODUCTOS POR ENCIMA DEL PROMEDIO.");
+            System.out.println("4.  Salir");
 
-                break;
+            menu = ingresoMenu.nextInt();
+            ingresoMenu.nextLine();
             
-            default:
-                System.out.println("opcion incorrecta");
+            switch (menu) {
+                case 1 -> {
+                    System.out.println("----CALCULAR EL PRECIO PROMEDIO DE LOS PRODUCTOS----");
+                    float suma = 0;
+
+                    for (int i = 0; i < 5; i++) {
+                        suma = precioProducto[i] + suma;
+                    }
+                    promedioPrecios = suma / 5;
+
+                    System.out.println("El promedio de los precios es: " + promedioPrecios);
+                }
+
+                case 2 -> {
+                    precioAlto = precioProducto[0];
+                    precioBajo = precioProducto[0];
+                    for (int i = 0; i < 5; i++) {
+                        if (precioProducto[i] > precioAlto) {
+                            precioAlto = precioProducto[i];
+                        }
+                        if (precioProducto[i] < precioBajo) {
+                            precioBajo = precioProducto[i];
+                        }
+                    }
+
+                    System.out.println("El precio alto es: " + precioAlto);
+                    System.out.println("El precio bajo es: " + precioBajo);
+                }
+
+                case 3 -> {
+                    contadorProductos=0;
+                    for (int i = 0; i < 5; i++) {
+                        if (precioProducto[i] > promedioPrecios) {
+                            contadorProductos=contadorProductos+1;
+                        }
+                    }
+                    System.out.println("Número de productos por encima del promedio: " + contadorProductos);
+                }
+                case 4 -> System.out.println("-----PROGRAMA FINALIZADO-----");
+
+                default -> System.out.println("opcion incorrecta");
+            }
         }
     }
 }
